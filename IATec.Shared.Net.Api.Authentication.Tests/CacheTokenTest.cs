cf946745676ae9663bb9d3bc0085645c856309d7
jonathan.jimenez <jonathan.jimenez@iatec.com>
2025-08-12 11:06:05 -0300

    Revert ":sparkles: feat: using testContainer to test the case using redis database"
    This reverts commit f250182460de978829fdbff645e123499b50df13.

--- IATec.Shared.Net.Api.Authentication.Tests/CacheTokenTest.cs
+++ IATec.Shared.Net.Api.Authentication.Tests/CacheTokenTest.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using Bogus;
 
 namespace IATec.Shared.Net.Api.Authentication.Tests;
-public class MemoryCacheTest
+public class CacheTokenTest
 {
     [Fact]
     public async Task TokenShouldStoreOnMemoryCache()
@@ -41,5 +41,45 @@ public class MemoryCacheTest
 
         //Assert
         Assert.Equal(tokenValue, result);
-    }    
+    }
+
+    [Fact]
+    public async Task TokenShouldStoreOnRedisCache()
+    {
+        //Arrange 
+        var faker = new Faker();
+
+        var inMemorySettings = new Dictionary<string, string>
+        {
+            {"Cache:UseDistributedCache", "false"},
+            {"Cache:RedisConnection", "localhost:6379,password=senha123,ssl=false,abortConnect=false,connectTimeout=10000,defaultDatabase=0"}
+        };
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings!)
+            .Build();
+
+        var services = new ServiceCollection();
+        services.AddCacheConfiguration(configuration);
+        services.AddScoped<TokenStoreService>();
+
+        var provider = services.BuildServiceProvider();
+
+        var tokenStore = provider.GetRequiredService<TokenStoreService>();
+
+        var tokenName = faker.Person.FirstName;
+        var tokenValue = faker.Random.Guid().ToString();
+
+        //Act
+        await tokenStore.StoreTokenAsync(
+            tokenName,
+            tokenValue                
+        );
+
+        var cache = provider.GetRequiredService<IDistributedCache>();
+
+        var result = await cache.GetStringAsync(tokenName);
+
+        //Assert
+        Assert.Equal(tokenValue, result);           
+    }
 }
